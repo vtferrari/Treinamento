7a5ecdd5b372ad6b67db6769eb2cc7f947ed18c3
Vinicius Temoteo Ferrari <vtferrari@hotmail.com>
2012-07-24 00:08:00 -0300

    agora vou comear o treinamento

---
+++ Exercicio001/Robozinho/src/robozinho/ControleRemoto.java
@@ -0,0 +1,12 @@
+package robozinho;
+
+/**
+ * o controle vai receber os comandos
+ * @author Vinicius
+ */
+class ControleRemoto {
+
+    public ControleRemoto() {
+    }
+    
+}
---
+++ Exercicio001/Robozinho/test/robozinho/ControleRemotoTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robozinho;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class ControleRemotoTest {
+    
+    public ControleRemotoTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
