cb65f06e85a0d7f34cc0c842ddfb782e5a1cec00
Vinicius Temoteo Ferrari <vtferrari@hotmail.com>
2012-10-10 19:01:46 -0300

    tentei

---
+++ Exercicio003/EditorHTML/src/option/None.java
@@ -0,0 +1,11 @@
+
+package option;
+
+public class None<T> extends Option<T> {
+
+    @Override
+    public T get() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ Exercicio005/Url/src/EzUrl/EzURL.java
@@ -0,0 +1,26 @@
+package EzUrl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class EzURL {
+
+    static String requestHTML(String path) throws MalformedURLException, IOException {
+
+        URL url = new URL(path);
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+        StringBuffer buffer = new StringBuffer();
+        String linha;
+        while ((linha = br.readLine()) != null) {
+            buffer.append(linha);
+        }
+        br.close();
+        System.out.println(buffer.toString());
+        return (buffer.toString());
+
+    }
+}
